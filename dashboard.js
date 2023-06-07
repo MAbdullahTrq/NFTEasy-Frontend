36c72c2558eb4d7011bf770cef05a8bb6fc91c57
Muhammad Abdullah <abdullahtariq1988@gmail.com>
2023-06-07 16:20:24 +0500

    Many Changes

--- dashboard.js
+++ dashboard.js
@@ -1,88 +1,144 @@
-// Array to store the collections
-let collections = [];
+// Configure Metamask as the provider
+import { ethers } from "./dist/ethers.min.js";
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+const signer = provider.getSigner();
 
-// Function to add a new collection
-function addCollection() {
-  const collectionCounter = collections.length + 1;
-  const collection = {
-    name: `Collection ${collectionCounter}`,
-    totalNFTs: 0,
-    soldNFTs: 0,
-    availableNFTs: 0,
-    image: `img/${collectionCounter}.jpg`
-  };
-  collections.push(collection);
-  renderCollectionBlocks();
-}
+// Contract address on Sepolia chain
+const contractAddress = "0xDD3dD692f42F7E2C43A93Bb3d60A0695D608D07b";
+const contractABI = [
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "contractAddress",
+				"type": "address"
+			}
+		],
+		"name": "ContractCreated",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "symbol",
+				"type": "string"
+			}
+		],
+		"name": "createContract",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "withdraw",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getBalance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getContractCount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			}
+		],
+		"name": "getContractsByOwner",
+		"outputs": [
+			{
+				"internalType": "address[]",
+				"name": "",
+				"type": "address[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]; // Add the ABI of your NFTEasyContractFactory
 
-// Function to mint a new NFT
-function mintNFT(index) {
-  const collection = collections[index];
-  collection.totalNFTs += 1;
-  collection.availableNFTs += 1;
-  renderCollectionBlocks();
-}
+// Contract instance
+let factoryContract;
 
-// Function to sell an NFT
-function sellNFT(index) {
-  const collection = collections[index];
-  if (collection.availableNFTs > 0) {
-    collection.availableNFTs -= 1;
-    collection.soldNFTs += 1;
+// Initialize function
+async function init() {
+  try {
+    // Request Metamask account access
+    await ethereum.request({ method: "eth_requestAccounts" });
+
+    // Create the contract instance
+    factoryContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+    // Render the initial collection blocks
     renderCollectionBlocks();
+  } catch (error) {
+    console.error("Error initializing:", error);
   }
 }
 
-// Function to render the collection blocks
-function renderCollectionBlocks() {
-  const collectionContainer = document.querySelector(".collection-container");
-  collectionContainer.innerHTML = "";
+// Function to add a new collection
+async function addCollection() {
+  const name = prompt("Enter the collection name:");
+  const symbol = prompt("Enter the collection symbol:");
 
-  collections.forEach((collection, index) => {
-    const collectionBlock = document.createElement("div");
-    collectionBlock.classList.add("collection-block");
+  // Check if name and symbol are provided
+  if (name && symbol) {
+    try {
+      // Call createContract function from NFTEasyContractFactory
+      const transaction = await factoryContract.createContract(name, symbol);
+      await transaction.wait();
 
-    const collectionImage = document.createElement("img");
-    collectionImage.src = collection.image;
-    collectionBlock.appendChild(collectionImage);
-
-    const collectionName = document.createElement("h2");
-    collectionName.innerText = collection.name;
-    collectionBlock.appendChild(collectionName);
-
-    const collectionDetails = document.createElement("p");
-    collectionDetails.innerText = `Total: ${collection.totalNFTs} | Sold: ${collection.soldNFTs} | Available: ${collection.availableNFTs}`;
-    collectionBlock.appendChild(collectionDetails);
-
-    const mintButton = document.createElement("button");
-    mintButton.innerText = "Mint NFT";
-    mintButton.addEventListener("click", () => {
-      mintNFT(index);
-    });
-    collectionBlock.appendChild(mintButton);
-
-    const sellButton = document.createElement("button");
-    sellButton.innerText = "Sell NFT";
-    sellButton.addEventListener("click", () => {
-      sellNFT(index);
-    });
-    collectionBlock.appendChild(sellButton);
-
-    collectionContainer.appendChild(collectionBlock);
-  });
+      // Add the collection to the UI or perform any other desired actions
+      renderCollectionBlocks();
+    } catch (error) {
+      console.error("Error adding collection:", error);
+    }
+  }
 }
 
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
-// Add event listener to the "Add Collection" button
-const addCollectionButton = document.querySelector(".add-collection-button");
-addCollectionButton.addEventListener("click", addCollection);
-
-// const backtoTopButton = document.querySelector(".back-to-button");
-// backtoTopButton.addEventListener("click", topFunction);
-
-// Render the initial collection blocks
-renderCollectionBlocks();
+// Rest of the code remains the same...
